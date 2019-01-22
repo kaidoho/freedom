8f5c3a2711004b3e261cf2e81678f76582649be1
kaidoho <kho237@gmail.com>
2019-01-22 17:48:58 -0500

    Inital commit for ArtyS7

---
+++ src/main/scala/everywhere/artyS7/System.scala
@@ -0,0 +1,50 @@
+// See LICENSE for license details.
+package sifive.freedom.everywhere.artyS7
+
+import Chisel._
+
+import freechips.rocketchip.config._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.devices.debug._
+import freechips.rocketchip.devices.tilelink._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.system._
+
+import sifive.blocks.devices.mockaon._
+import sifive.blocks.devices.gpio._
+import sifive.blocks.devices.pwm._
+import sifive.blocks.devices.spi._
+import sifive.blocks.devices.uart._
+import sifive.blocks.devices.i2c._
+
+//-------------------------------------------------------------------------
+// ArtyS7DevKitSystem
+//-------------------------------------------------------------------------
+
+class ArtyS7DevKitSystem(implicit p: Parameters) extends RocketSubsystem
+    with HasPeripheryMaskROMSlave
+    with HasPeripheryDebug
+    with HasPeripheryMockAON
+    with HasPeripheryUART
+    with HasPeripherySPIFlash
+    with HasPeripherySPI
+    with HasPeripheryGPIO
+    with HasPeripheryPWM
+    with HasPeripheryI2C {
+  override lazy val module = new ArtyS7DevKitSystemModule(this)
+}
+
+class ArtyS7DevKitSystemModule[+L <: ArtyS7DevKitSystem](_outer: L)
+  extends RocketSubsystemModuleImp(_outer)
+    with HasPeripheryDebugModuleImp
+    with HasPeripheryUARTModuleImp
+    with HasPeripherySPIModuleImp
+    with HasPeripheryGPIOModuleImp
+    with HasPeripherySPIFlashModuleImp
+    with HasPeripheryMockAONModuleImp
+    with HasPeripheryPWMModuleImp
+    with HasPeripheryI2CModuleImp {
+  // Reset vector is set to the location of the mask rom
+  val maskROMParams = p(PeripheryMaskROMKey)
+  global_reset_vector := maskROMParams(0).address.U
+}
